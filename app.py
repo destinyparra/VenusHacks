56e598305d0a3b03f41aff2869155dab8245c7ef
parbonichattopadhyay <parboni845@gmail.com>
2025-05-24 14:48:18 -0700

    updated home page

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
 
 @app.route('/')
 def home():
@@ -10,6 +15,7 @@ def home():
 @app.route('/map')
 def map_view():
     return render_template('map.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
