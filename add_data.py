ac3d17d7bc157798131bae7b217f9dd23804d0ec
falakchhatre <fchhatre@uci.edu>
2025-05-24 18:30:15 -0700

    updated database

--- add_data.py
+++ add_data.py
@@ -1,17 +1,53 @@
 from app import app, db, Location
 from datetime import datetime, timezone
+import random
+
+locations_raw = [
+    {"name": "Aldrich Hall - Basement", "latitude": 33.64841625857725, "longitude": -117.84122708476183},
+    {"name": "Aldrich Hall - 2nd floor, 214", "latitude": 33.64841625857725, "longitude": -117.84122708476183},
+    {"name": "Aldrich Hall - 2nd floor, 216", "latitude": 33.64841625857725, "longitude": -117.84122708476183},
+    {"name": "Aldrich Hall - 6th floor, 601", "latitude": 33.64841625857725, "longitude": -117.84122708476183},
+    {"name": "Aldrich Hall - 6th floor, 604", "latitude": 33.64841625857725, "longitude": -117.84122708476183},
+    {"name": "Anteater Learning Pavilion - 1st Floor", "latitude": 33.647189306833425, "longitude": -117.84462947326799},
+    {"name": "Anteater Learning Pavilion - 2nd Floor", "latitude": 33.647189306833425, "longitude": -117.84462947326799},
+    {"name": "Anteater Learning Pavilion - 3rd Floor", "latitude": 33.647189306833425, "longitude": -117.84462947326799},
+    {"name": "Biological Sciences III - Basement", "latitude": 33.64552858342245, "longitude": -117.84571966652165},
+    {"name": "Biological Sciences III - Level 1", "latitude": 33.64552858342245, "longitude": -117.84571966652165},
+    {"name": "Anteater Recreation Center - 1st floor, 104", "latitude": 33.643655696764164, "longitude": -117.82791864570751},
+    {"name": "Anteater Recreation Center - 1st floor, 105", "latitude": 33.643655696764164, "longitude": -117.82791864570751},
+    {"name": "Anteater Recreation Center - 2nd floor, 272", "latitude": 33.643655696764164, "longitude": -117.82791864570751},
+    {"name": "Anteater Recreation Center - 2nd Floor, 275", "latitude": 33.643655696764164, "longitude": -117.82791864570751},
+]
+
+availability_options = ["Low", "Medium", "Stocked"]
+
+def extract_floor(name):
+    parts = name.split(" - ")
+    if len(parts) > 1:
+        return parts[1]
+    return None
+
+def assign_location_group(name):
+    return name.split(" - ")[0]
 
 with app.app_context():
-    new_location = Location(
-        name="Aldrich Hall - basement",
-        latitude=33.64841625857725,
-        longitude=-117.84122708476183,
-        pads_availability="Stocked",    
-        tampons_availability="Stocked",
-        free=True,
-        accessibility=True,
-        last_updated=datetime.now(timezone.utc)
-    )
+    db.drop_all()
+    db.create_all()
 
-    db.session.add(new_location)
+    for loc in locations_raw:
+        location = Location(
+            name=loc["name"],
+            latitude=loc["latitude"],
+            longitude=loc["longitude"],
+            floor=extract_floor(loc["name"]),
+            location_group=assign_location_group(loc["name"]),
+            pads_availability=random.choice(availability_options),
+            tampons_availability=random.choice(availability_options),
+            free=random.choice([True, False]),
+            accessibility=random.choice([True, False]),
+            last_updated=datetime.now(timezone.utc)
+        )
+        db.session.add(location)
+
     db.session.commit()
+    print(f"Inserted {len(locations_raw)} new locations with auto-incremented IDs.")
--- app.py
+++ app.py
@@ -15,14 +15,17 @@ db = SQLAlchemy(app)
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    location_group = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(150), nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+    floor = db.Column(db.String(50), nullable=True)
     pads_availability = db.Column(db.String(10), default='Low')
     tampons_availability = db.Column(db.String(10), default='Low')
     free = db.Column(db.Boolean, nullable=False)
     accessibility = db.Column(db.Boolean, nullable=True)
     last_updated = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
 
 with app.app_context():
     db.create_all()
@@ -30,6 +33,12 @@ with app.app_context():
 @app.route('/') #READ
 def home():
     locations = Location.query.all()
+    grouped_locations = {}
+    for loc in locations:
+        if loc.location_group not in grouped_locations:
+            grouped_locations[loc.location_group] = []
+            grouped_locations[loc.location_group].append(loc)
+
     return render_template('home.html', locations=locations)
 
 @app.route('/update_stock/<int:location_id>', methods=['POST']) #UPDATE
@@ -44,20 +53,6 @@ def update_stock(location_id):
     db.session.commit()
     return jsonify({"success": True})
 
-'''
-@app.route('/add_location', methods=['GET', 'POST'])
-def add_location():
-    if request.method == 'POST':
-        name = request.form['name']
-        latitude = float(request.form['latitude'])
-        longitude = float(request.form['longitude'])
-        new_location = Location(name=name, latitude=latitude, longitude=longitude)
-        db.session.add(new_location)
-        db.session.commit()
-        return redirect(url_for('home'))
-
-    return render_template('add_location.html')
-'''
 
 @app.route('/map')
 def map_view():
@@ -68,16 +63,20 @@ def locations():
     all_locations = Location.query.all()
     locations_data = [{
         'id': loc.id,
+        'location_group' : loc.location_group,
         'name': loc.name,
         'latitude': loc.latitude,
         'longitude': loc.longitude,
+        'floor' : loc.floor,
         'pads_availability': getattr(loc, 'pads_availability', 'N/A'),
         'tampons_availability': getattr(loc, 'tampons_availability', 'N/A'),
         'free' : loc.free,
         'accessibility': loc.accessibility,
         'last_updated': loc.last_updated.strftime('%Y-%m-%d %H:%M:%S') if loc.last_updated else ''
     } for loc in all_locations]
+    
     return jsonify(locations_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
